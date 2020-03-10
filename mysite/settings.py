bf63f20852bdfdc7c3537d19853ab1e666cc8866
articuly <articuly@gmail.com>
2020-03-10 23:48:45 +0800

    文章列表与详情页面

--- mysite/settings.py
+++ mysite/settings.py
@@ -53,8 +53,8 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': False,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
